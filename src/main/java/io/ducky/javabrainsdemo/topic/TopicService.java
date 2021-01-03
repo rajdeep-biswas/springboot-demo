3fe29ffe64f32e69cce40eb16b716b2b8cef25d1
unknown <you@example.com>
2021-01-03 22:19:01 +0530

    improved comment

---
+++ src/main/java/io/ducky/javabrainsdemo/topic/TopicService.java
@@ -0,0 +1,36 @@
+package io.ducky.javabrainsdemo.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	// this happens to be a static initialization block
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "spring framework", "yada yada 1"),
+			new Topic("java", "core java", "yada yada 2"),
+			new Topic("js", "shitcake", "yada yada 3")
+	);
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		/*
+		 *  added the try-catch block so it doesn't return
+		 *  a Whitelabel error page when invalid topic ID is entered.
+		 *  can try a better alternative with a redirect in the TopicController class, later
+		 */
+		try {
+			// this syntax, god damn
+			return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		}
+		catch(Exception e) {
+			return new Topic("topic", "wasn't", "found");
+		}
+	}
+}

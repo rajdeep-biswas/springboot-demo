dae146a258f4c70c9bcff8381867b5554d0582ba
unknown <you@example.com>
2021-01-03 19:58:17 +0530

    business service 1

--- src/main/java/io/ducky/javabrainsdemo/topic/TopicController.java
+++ src/main/java/io/ducky/javabrainsdemo/topic/TopicController.java
@@ -3,19 +3,18 @@ package io.ducky.javabrainsdemo.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		
-		return Arrays.asList(
-				new Topic("spring", "spring framework", "yada yada 1"),
-				new Topic("java", "core java", "yada yada 2"),
-				new Topic("js", "shitcake", "yada yada 3")
-		);
+		return topicService.getAllTopics();
 	}
 }

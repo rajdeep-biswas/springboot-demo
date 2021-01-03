068fdea4c6014dc7e8f750ea64b257c5b659c0e4
unknown <you@example.com>
2021-01-03 22:15:44 +0530

    path variable

--- src/main/java/io/ducky/javabrainsdemo/hello/HelloController.java
+++ src/main/java/io/ducky/javabrainsdemo/hello/HelloController.java
@@ -1,5 +1,6 @@
 package io.ducky.javabrainsdemo.hello;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String sayHi() {
 		return "Hi";
+	}
+	
+	@RequestMapping("/hello/{name}")
+	public String sayHiPerson(@PathVariable String name) {
+		return "Hi, " + name + "!";
 	}
 }
